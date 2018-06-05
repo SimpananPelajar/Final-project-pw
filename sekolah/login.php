0f815cfadb020c1dbdc66743ca30f5baa6814ca9
16110344 <ridho.bimo@students.amikom.ac.id>
2018-06-05 22:06:52 +0700

    Halaman User

---
+++ sekolah/login.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Login - Simpanan Pelajar</title>
+	<meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+	<link rel="stylesheet" href="assets/bower_components/bootstrap/dist/css/bootstrap.min.css">
+	<link rel="stylesheet" href="assets/bower_components/font-awesome/css/font-awesome.min.css">
+	<link rel="stylesheet" href="assets/bower_components/Ionicons/css/ionicons.min.css">
+	<link rel="stylesheet" href="assets/dist/css/AdminLTE.min.css">
+	<link rel="stylesheet" href="assets/plugins/iCheck/square/blue.css">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
+</head>
+<body class="hold-transition login-page">
+	<div class="login-box">
+		<div class="login-logo">
+			<a href="login.php"><b>SIM</b>PEL</a>
+			 <!-- <a href=""><img src="assets/dist/img/avatar.png"></a> -->
+		</div>
+		<div class="login-box-body">
+			<p class="login-box-msg">Masukkan email dan password anda</p>
+
+			<form action="dashboard.php" method="post">
+				<div class="form-group has-feedback">
+					<input type="email" class="form-control" placeholder="Email" required>
+					<span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+				</div>
+				<div class="form-group has-feedback">
+					<input type="password" class="form-control" placeholder="Password" required>
+					<span class="glyphicon glyphicon-lock form-control-feedback"></span>
+				</div>
+				<div class="row">
+					<div class="col-xs-12">
+						<button type="submit" class="btn btn-primary btn-block btn-flat" style="width: 100%">Masuk</button>
+					</div>
+				</div>
+			</form>
+			<div class="social-auth-links text-center">
+				<p>- OR -</p>
+				<a href="#" class="btn btn-block btn-social btn-facebook btn-flat"><i class="fa fa-facebook"></i> Masuk dengan
+				Facebook</a>
+				<a href="#" class="btn btn-block btn-social btn-google btn-flat"><i class="fa fa-google-plus"></i> Masuk dengan
+				Google+</a>
+			</div>
+			<a href="#">Lupa password</a><br>
+			<a href="register.php" class="text-center">Register a new membership</a>
+		</div>
+	</div>
+	<script src="assets/bower_components/jquery/dist/jquery.min.js"></script>
+	<script src="assets/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+	<script src="assets/plugins/iCheck/icheck.min.js"></script>
+	<script>
+		$(function () {
+			$('input').iCheck({
+				checkboxClass: 'icheckbox_square-blue',
+				radioClass: 'iradio_square-blue',
+      			increaseArea: '20%' // optional
+  			});
+		});
+	</script>
+</body>
+</html>
